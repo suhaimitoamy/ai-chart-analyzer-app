c7aa2e92ed591a2b906c3400aa0d1a804c11ca70
suhaimitoamy <34860608+suhaimitoamy@users.noreply.github.com>
2026-06-19 20:45:12 +0800

    Apply Kotlin Android plugin to app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
   alias(libs.plugins.android.application)
+  alias(libs.plugins.kotlin.android)
   alias(libs.plugins.kotlin.compose)
   alias(libs.plugins.google.devtools.ksp)
   alias(libs.plugins.roborazzi)
@@ -41,6 +42,10 @@ android {
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
+  }
+
+  kotlinOptions {
+    jvmTarget = "11"
   }
 
   buildFeatures {
