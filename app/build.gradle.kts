d7d94e19e831c4a21c44237c2c8f72444394b43a
suhaimitoamy <suhaimitoamy@gmail.com>
2026-06-19 20:29:08 +0800

    Fix compileSdk to 36, update Kotlin/KSP versions to resolve NPE, add generated app icon PNG

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
   namespace = "com.example"
-  compileSdk = 35
+  compileSdk = 36
 
   defaultConfig {
     applicationId = "com.aistudio.aitradingbot.dkajsn"
     minSdk = 24
-    targetSdk = 35
+    targetSdk = 36
     versionCode = 1
     versionName = "1.0"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
